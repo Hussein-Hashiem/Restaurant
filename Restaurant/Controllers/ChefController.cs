8127db35647d2897441e4f86e0b0c22bcf4792bc
Nouran Ahmed <ahmednoran200475@gmail.com>
2025-09-26 01:54:14 +0300

    Add initial version for controllers-to be tested

---
+++ Restaurant/Controllers/ChefController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurnat.BLL.Services.Apstraction;
+using Restaurnat.BLL.ModelVM.Payment;
+using Restaurnat.BLL.ModelVM.Chef;
+using Restaurnat.BLL.Services.Implementation;
+using Restaurnat.BLL.ModelVM.User;
+using Restaurnat.DAL.Entities;
+namespace Restaurant.PL.Controllers
+{
+    public class ChefController : Controller
+    {
+        private readonly IChefService cservice;
+        public ChefController(IChefService chservice)
+        {
+            this.cservice = chservice;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveChef(CreateChefVM chef, IFormFile img)
+        {
+            if (!ModelState.IsValid)
+            {
+                var result = cservice.Create(chef, img);
+                if (result.Item1)
+                {
+                    return RedirectToAction("GetAll", "Chef");
+                }
+
+            }
+            return View(chef);
+        }
+        public IActionResult GetAll()
+        {
+            var chefs= cservice.GetAll();
+
+            if (chefs.Item2 != null)
+            {
+                ViewBag.Message(chefs.Item2);
+            }
+            return View(chefs.Item1);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var result = cservice.Delete(id);
+
+            if (result.Item2 != null)
+            {
+                ViewBag.Message(result.Item2);
+            }
+            return RedirectToAction("GetAll", "Chef");
+        }
+        public IActionResult GetById(int id)
+        {
+            var result = cservice.GetById(id);
+
+            if (result.Item2 != null)
+            {
+                ViewBag.Message(result.Item2);
+                return RedirectToAction("GetAll", "Chef");
+            }
+            return View(result.Item1);
+        }
+        public IActionResult Edit(EditChefVM chef, IFormFile img)
+        {
+            var result = cservice.Update(chef, img);
+
+            if (result.Item2 != null)
+            {
+                ViewBag.Message(result.Item2);
+                return RedirectToAction("GetAll", "Chef");
+            }
+            return View(result.Item1);
+        }
+    }
+}
---
+++ Restaurant/Controllers/EventController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurnat.BLL.ModelVM.Chef;
+using Restaurnat.BLL.ModelVM.Event;
+using Restaurnat.BLL.Services.Apstraction;
+
+namespace Restaurant.PL.Controllers
+{
+    public class EventController : Controller
+    {
+      
+        private readonly IEventService eservice;
+        public EventController(IEventService evservice)
+        {
+            this.eservice = evservice;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveChef(CreateEventVM ev)
+        {
+            if (!ModelState.IsValid)
+            {
+                var result = eservice.Create(ev);
+                if (result.Item1)
+                {
+                    return RedirectToAction("GetAll", "Event");
+                }
+
+            }
+            return View(ev);
+        }
+        public IActionResult GetAll()
+        {
+            var events = eservice.GetAllEvents();
+
+            if (events.Item2 != null)
+            {
+                ViewBag.Message(events.Item2);
+            }
+            return View(events.Item1);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var result = eservice.Delete(id);
+
+            if (result.Item2 != null)
+            {
+                ViewBag.Message(result.Item2);
+            }
+            return RedirectToAction("GetAll", "Event");
+        }
+        public IActionResult GetById(int id)
+        {
+            var result = eservice.GetById(id);
+
+            if (result.Item2 != null)
+            {
+                ViewBag.Message(result.Item2);
+                return RedirectToAction("GetAll", "Event");
+            }
+            return View(result.Item1);
+        }
+        public IActionResult Edit(EditEventVM ev)
+        {
+            var result = eservice.Update(ev);
+
+            if (result.Item2 != null)
+            {
+                ViewBag.Message(result.Item2);
+                return RedirectToAction("GetAll", "Chef");
+            }
+            return View(result.Item1);
+        }
+    }
+}
